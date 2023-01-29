34ea6788fe85f0ca9ca081fffec0c7534fe53a7d
ibtisamim <ibtisam_im@yahoo.com>
2023-01-29 17:51:34 +0300

    update

--- lib/widgets/trendingMovies.dart
+++ lib/widgets/trendingMovies.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tmdb_api/tmdb_api.dart';
 import 'package:vmovies/HomePage.dart';
+
+import '../styles/textStyle.dart';
 
 class TrendingMovies extends StatefulWidget {
   final List trending;
@@ -16,14 +21,46 @@ class TrendingMovies extends StatefulWidget {
 }
 
 class TrendingMoviesState extends State<TrendingMovies> {
+  int _currentPage = 0;
+  late Timer _timer;
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 4), (Timer timer) {
+      if (_currentPage < 2) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration: Duration(milliseconds: 350),
+        curve: Curves.easeIn,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+        body: ListView(children: [
+      Column(
         children: [
           Container(
             height: 560,
             child: PageView.builder(
+                controller: _pageController,
                 itemCount: widget.trending.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (BuildContext context, int index) {
@@ -43,15 +80,15 @@ class TrendingMoviesState extends State<TrendingMovies> {
                 }),
           ),
           Container(
-            height: 300,
+            height: 500,
             decoration: const BoxDecoration(
-                color: Colors.red,
+                color: Color.fromARGB(255, 255, 255, 255),
                 borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20))),
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50))),
           )
         ],
       ),
-    );
+    ]));
   }
 }
